dd93fd79cd680d854361ade568f9e25d982bc6b8
Gabe Mifflen <115088238+gmifflen@users.noreply.github.com>
2024-03-31 01:30:35 -0300

    changed code formatting to taste. added main() function

--- build.py
+++ build.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 import sublate as sub
 
-sub.data.update({
-    "date": sub.date_iso(),
-    "colors": sub.read("colors/*.yaml").values(),
-})
 
-sub.rm("output")
-sub.cp("theme", "output")
-sub.run("output/*/build.py")
+def main():
+    # update data dictionary with the current date and color themes
+    sub.data.update(
+        {
+            "date": sub.date_iso(),
+            "colors": sub.read("colors/*.yaml").values(),
+        }
+    )
+
+    # clean output directory and copy the theme templates over
+    sub.rm("output")
+    sub.cp("theme", "output")
+
+    # execute build scripts themes in the output directory
+    sub.run("output/*/build.py")
+
+
+if __name__ == "__main__":
+    main()
